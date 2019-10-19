5c51f71247a74075c0e5c7876e5b44bc79c43119
Max Blasdel <maxblasdel@gmail.com>
2019-10-18 20:40:51 -0700

    raster data table function

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,42 @@
-# Hello, world!
-# This is an example function named 'hello' 
+#' Transform raster to data.table
+#' 
+#' @param x  Raster* object
+#' @param row.names	`NULL` or a character vector giving the row names for the data frame. Missing values are not allowed
+#' @param optional	logical. If `TRUE`, setting row names and converting column names (to syntactic names: see make.names) is optional
+#' @param xy  logical. If `TRUE`, also return the spatial coordinates
+#' @param centroids	logical. If TRUE return the centroids instead of all spatial coordinates (only relevant if xy=TRUE)
+#' @param sepNA	logical. If TRUE the parts of the spatial objects are separated by lines that are NA (only if xy=TRUE and, for polygons, if centroids=FALSE
+#' @param ...	 Additional arguments (none) passed to `raster::as.data.frame`
+#' 
+#' @value returns a data.table object
+#' @examples
+#' logo <- brick(system.file("external/rlogo.grd", package="raster"))
+#' v <- as.data.table(logo)
+#' @import
 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+as.data.table.raster <- function(x, row.names = NULL, optional = FALSE, xy=FALSE, inmem = canProcessInMemory(x, 2), ...) {
+  stopifnot(require("data.table"))
+  if(inmem) {
+    v <- as.data.table(as.data.frame(x, row.names=row.names, optional=optional, xy=xy, ...))
+  } else {
+    tr <- blockSize(x, n=2)
+    l <- lapply(1:tr$n, function(i) 
+      as.data.table(as.data.frame(getValues(x, 
+                                            row=tr$row[i], 
+                                            nrows=tr$nrows[i]), 
+                                  row.names=row.names, optional=optional, xy=xy, ...)))
+    v <- rbindlist(l)
+  }
+  coln <- names(x)
+  if(xy) coln <- c("x", "y", coln)
+  setnames(v, coln)
+  v
 }
+
+if (!isGeneric("as.data.table")) {
+  setGeneric("as.data.table", function(x, ...)
+    standardGeneric("as.data.table"))
+}  
+
+setMethod('as.data.table', signature(x='data.frame'), data.table::as.data.table)
+setMethod('as.data.table', signature(x='Raster'), as.data.table.raster)
